59f22518d2be9e84489947f9c9d097e278ee5b3e
Zhu Liang <zhuliangg11@gmail.com>
2022-11-03 20:01:05 +0800

    feat(doc): terms, refactor

--- index.js
+++ index.js
@@ -6,10 +6,38 @@ const token = process.env.TELEGRAM_TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-const linkMap = {
+const definitionMap = {
   rsu: [
     'Restricted Stock Unit',
     'https://www.investopedia.com/terms/r/restricted-stock-unit.asp',
+  ],
+  lc: [
+    'LC is an acronym for LeetCode, an online judge (OJ) for practicing coding problems and prepare for technical interviews.',
+    'https://leetcode.com/',
+  ],
+  tc: [
+    'TC refers to the total compensation of a job in a year. Total compensation in a tech company typically consists of 3 components: base salary, bonus and stock (or RSU / stock options).',
+  ],
+  ic: [
+    'IC is an acronym for individual contributor. Individual contributor is one of the two tracks for software engineering career, the other being the management track.',
+  ],
+  lgtm: [
+    'The letters LGTM stand for "looks good to me." Those working in software development often use it after reviewing code.',
+  ],
+  pr: [
+    "PR/MR means pull request or merge request. They tell others about changes you've pushed to a branch in a repository. They are essentially a request to merge one branch into another.",
+  ],
+  mr: [
+    "PR/MR means pull request or merge request. They tell others about changes you've pushed to a branch in a repository. They are essentially a request to merge one branch into another.",
+  ],
+  swe: [
+    'SWE is an acronym for software engineer, a profession where people code and write software.',
+  ],
+  em: [
+    'EM is an acronym for engineering manager. Engineering manager (management) is one of the two tracks for software engineering career, the other being the individual contributor track.',
+  ],
+  pip: [
+    'In tech companies, PIP means performance improvement plan. The PIP program is for people whose performance did not meet expectations in a review cycle. If PIP goals are not met at the end of the program, the employee will usually be let go.',
   ],
 };
 
@@ -27,24 +55,13 @@ bot.onText(/!bot (.+)/, (msg, match) => {
   console.log(`Received: ${resp}`);
 
   let reply = `Sorry, I have not learnt about ${resp} yet.`;
-  if (linkMap[resp.toLowerCase()]) {
-    const [name, link] = linkMap[resp.toLowerCase()];
-    reply = `${resp}: ${name}. Read more: ${link}`;
-  } else if (resp.toLowerCase() === 'TC'.toLowerCase()) {
-    reply =
-      'TC refers to the total compensation of a job in a year. Total compensation in a tech company typically consists of 3 components: base salary, bonus and stock (or RSU / stock options).';
-  } else if (resp.toLowerCase() === 'IC'.toLowerCase()) {
-    reply =
-      'IC is an acronym for individual contributor. Individual contributor is one of the two tracks for software engineering career, the other being the management track.';
-  } else if (resp.toLowerCase() === 'lgtm'.toLowerCase()) {
-    reply =
-      'The letters LGTM stand for "looks good to me." Those working in software development often use it after reviewing code.';
-  } else if (
-    resp.toLowerCase() === 'PR'.toLowerCase() ||
-    resp.toLowerCase() === 'MR'.toLowerCase()
-  ) {
-    reply =
-      "PR/MR means pull request or merge request. They tell others about changes you've pushed to a branch in a repository. They are essentially a request to merge one branch into another.";
+  if (definitionMap[resp.toLowerCase()]) {
+    const [description, link] = definitionMap[resp.toLowerCase()];
+    if (link) {
+      reply = `${resp}: ${description}. Read more: ${link}`;
+    } else {
+      reply = `${description}`;
+    }
   }
 
   console.log(`Reply: ${reply}`);
