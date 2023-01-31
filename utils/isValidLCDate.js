431ac0ac70d8b46a2f2e8b66488804133b4b0985
kk-min <76023265+kk-min@users.noreply.github.com>
2023-01-31 13:47:01 +0800

    Update variable names to explicitly reflect meaning

--- utils/isValidLCDate.js
+++ utils/isValidLCDate.js
@@ -4,20 +4,31 @@ let utc = require('dayjs/plugin/utc');
 dayjs.extend(isBetween);
 dayjs.extend(utc);
 
-function isValidLCDate(resp, mockDate, mockHour) {
+function isValidLCDate(
+  submittedDateString,
+  mockServerDateString,
+  mockServerHourString
+) {
   let submissionHour, leftBound, rightBound;
-  if (mockDate == undefined && mockHour == undefined) {
+  if (mockServerDateString == undefined && mockServerHourString == undefined) {
+    // Use actual server date and hour
     leftBound = dayjs.utc().hour(0).minute(0).second(0).millisecond(0);
     rightBound = leftBound.add(1, 'day');
     submissionHour = dayjs.utc().hour();
   } else {
-    // For tests. Note that both mockDate and mockHour should be passed in.
-    leftBound = dayjs.utc(mockDate, 'YYYYMMDD').hour(0).minute(0).second(0);
+    // For tests where we want to mock the server time. Note that both mockServerDateString and mockServerHourString should be passed in.
+    leftBound = dayjs
+      .utc(mockServerDateString, 'YYYYMMDD')
+      .hour(0)
+      .minute(0)
+      .second(0);
     rightBound = leftBound.add(1, 'day');
-    submissionHour = mockHour;
+    submissionHour = mockServerHourString;
   }
 
-  const submissionDate = dayjs.utc(resp, 'YYYYMMDD').hour(submissionHour);
+  const submissionDate = dayjs
+    .utc(submittedDateString, 'YYYYMMDD')
+    .hour(submissionHour);
   //  console.log(`Submission hour: ${submissionHour}`);
   //  console.log(`Submission date: ${submissionDate}`);
   //  console.log(`Left bound: ${leftBound}`);
