5e19e1e9963cd3ddad89dbe4d19f936411194946
kk-min <76023265+kk-min@users.noreply.github.com>
2023-02-04 17:43:33 +0800

    Remove mockServerHour

--- test/isValidLCDate.test.js
+++ test/isValidLCDate.test.js
@@ -1,52 +1,44 @@
 const isValidLCDate = require('../utils/isValidLCDate');
 
 let mockServerDate;
-let mockServerHour;
 
 test('Valid LC Date', () => {
   mockServerDate = '20210121';
-  mockServerHour = 0;
-  expect(isValidLCDate('20210121', mockServerDate, mockServerHour)).toBe(true);
+  expect(isValidLCDate('20210121', mockServerDate)).toBe(true);
 });
 
 test('Valid LC Date', () => {
   mockServerDate = '20210126';
-  mockServerHour = 3;
-  expect(isValidLCDate('20210126', mockServerDate, mockServerHour)).toBe(true);
+  expect(isValidLCDate('20210126', mockServerDate)).toBe(true);
 });
 
 test('Valid LC Date', () => {
   mockServerDate = '20210120';
-  mockServerHour = 10;
-  expect(isValidLCDate('20210120', mockServerDate, mockServerHour)).toBe(true);
+  expect(isValidLCDate('20210120', mockServerDate)).toBe(true);
 });
 
 test('Invalid LC Date', () => {
   mockServerDate = '20210123';
-  mockServerHour = 23;
-  expect(isValidLCDate('20210124', mockServerDate, mockServerHour)).toBe(false);
+  expect(isValidLCDate('20210124', mockServerDate)).toBe(false);
 });
 
 test('Invalid LC Date', () => {
   mockServerDate = '20210127';
   mockhour = 0;
-  expect(isValidLCDate('20210126', mockServerDate, mockServerHour)).toBe(false);
+  expect(isValidLCDate('20210126', mockServerDate)).toBe(false);
 });
 
 test('Invalid LC Date', () => {
   mockServerDate = '20210124';
-  mockServerHour = 0;
-  expect(isValidLCDate('20210125', mockServerDate, mockServerHour)).toBe(false);
+  expect(isValidLCDate('20210125', mockServerDate)).toBe(false);
 });
 
 test('Invalid LC Date', () => {
   mockServerDate = '20210122';
-  mockServerHour = 0;
-  expect(isValidLCDate('20210120', mockServerDate, mockServerHour)).toBe(false);
+  expect(isValidLCDate('20210120', mockServerDate)).toBe(false);
 });
 
 test('Invalid LC Date', () => {
   mockServerDate = '20210124';
-  mockServerHour = 0;
-  expect(isValidLCDate('20210126', mockServerDate, mockServerHour)).toBe(false);
+  expect(isValidLCDate('20210126', mockServerDate)).toBe(false);
 });
--- utils/isValidLCDate.js
+++ utils/isValidLCDate.js
@@ -4,28 +4,23 @@ let utc = require('dayjs/plugin/utc');
 dayjs.extend(isBetween);
 dayjs.extend(utc);
 
-function isValidLCDate(submittedDateString, mockServerDate, mockServerHour) {
-  let submissionHour, leftBound, rightBound;
-  if (mockServerDate == undefined && mockServerHour == undefined) {
+function isValidLCDate(submittedDateString, mockServerDate) {
+  let leftBound, rightBound;
+  if (mockServerDate == undefined) {
     // Use actual server date and hour
     leftBound = dayjs.utc().hour(0).minute(0).second(0).millisecond(0);
     rightBound = leftBound.add(1, 'day');
-    submissionHour = dayjs.utc().hour();
   } else {
-    // For tests where we want to mock the server time. Note that both mockServerDate and mockServerHour should be passed in.
+    // For tests where we want to mock the server time.
     leftBound = dayjs
       .utc(mockServerDate, 'YYYYMMDD')
       .hour(0)
       .minute(0)
       .second(0);
     rightBound = leftBound.add(1, 'day');
-    submissionHour = mockServerHour;
   }
 
-  const submissionDate = dayjs
-    .utc(submittedDateString, 'YYYYMMDD')
-    .hour(submissionHour);
-  //  console.log(`Submission hour: ${submissionHour}`);
+  const submissionDate = dayjs.utc(submittedDateString, 'YYYYMMDD');
   //  console.log(`Submission date: ${submissionDate}`);
   //  console.log(`Left bound: ${leftBound}`);
   //  console.log(`Right bound: ${rightBound}`);
